354aa79d384c24a13f6690e5a1c1863e3a85f923
Tru Lie <84538219+TruLie13@users.noreply.github.com>
2024-10-01 13:31:40 -0600

    changes for hosting on FB

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfP_6mIYwjGK0dxwUdkh1_SD_ruxzhuMU",
+  authDomain: "star-grabber.firebaseapp.com",
+  projectId: "star-grabber",
+  storageBucket: "star-grabber.appspot.com",
+  messagingSenderId: "843232728327",
+  appId: "1:843232728327:web:091e19c3a8df64b3f9e1a2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
